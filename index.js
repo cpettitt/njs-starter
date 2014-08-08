ce4a81a5f4f9826e05fee52a986e1f101a6c7374
Chris Pettitt <cpettitt@gmail.com>
2014-08-08 06:48:33 -0700

    Generate license file from input

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var promzard = require('promzard'),
     fs = require('fs'),
     path = require('path'),
+    _ = require('underscore'),
+    cwd = process.cwd(),
     pkgIn = path.join(__dirname, 'lib/input.js'),
-    pkgOut = path.join(process.cwd(), 'package.json');
+    pkgOut = path.join(cwd, 'package.json');
 
 if (fs.existsSync(pkgOut)) {
-  console.error('ERROR: ' + path.basename(pkgOut) + ' already exists!');
-  process.exit(1);
+  bail('ERROR: ' + path.basename(pkgOut) + ' already exists!');
 }
 
 var ctx = {
@@ -17,7 +18,38 @@ var ctx = {
 promzard(pkgIn, ctx, function(err, data) {
   if (err) { throw err; }
 
-  var pkgContents = {
+  validate(data);
+
+  writePackageJson(data);
+  copyLicenseFile(data);
+
+  console.log('Done.');
+});
+
+function bail(msg) {
+  console.error(msg);
+  process.exit(1);
+}
+
+function validate(data) {
+  if (!fs.existsSync(getLicenseFile(data.license))) {
+    bail('ERROR: A license file for ' + data.license + ' does not exist.\n'+
+         'Missing: ' + getLicenseFile(data.license));
+  }
+}
+
+function getLicenseFile(license) {
+  return path.join(__dirname, 'templates/license', 'LICENSE.' + license);
+}
+
+function copyLicenseFile(data) {
+  var licenseTemplate = fs.readFileSync(getLicenseFile(data.license), 'UTF-8'),
+      license = _.template(licenseTemplate, data);
+  fs.writeFileSync(path.join(cwd, 'LICENSE'), license); 
+}
+
+function writePackageJson(data) {
+  var contents = {
     name: data.name,
     version: '0.0.1-pre',
     description: data.description,
@@ -26,8 +58,5 @@ promzard(pkgIn, ctx, function(err, data) {
     license: data.license
   };
 
-  fs.writeFile(pkgOut, JSON.stringify(pkgContents, null, 2), function(err) {
-    if (err) { throw err; }
-    console.log('Done.');
-  });
-});
+  fs.writeFileSync(pkgOut, JSON.stringify(contents, null, 2));
+}
